6729b5ad7e95bef00bf0be8c96f37009f6ccecca
nikosev <nikos.ev@hotmail.com>
2022-11-07 15:43:18 +0200

    Initialize `webOrigins` to `+`

--- main.py
+++ main.py
@@ -169,7 +169,7 @@ def sync(dry_run):
 
 
 def format_keycloak_client_object(msg, realm_default_client_scopes):
-    json_template = '{"attributes":{"client_credentials.use_refresh_token":"false","oauth2.device.authorization.grant.enabled":"false","oauth2.token.exchange.grant.enabled":false,"oidc.ciba.grant.enabled":"false","refresh.token.max.reuse":"0","revoke.refresh.token":"false","use.jwks.string":"false","use.jwks.url":"false","use.refresh.tokens":"false"},"consentRequired":false,"implicitFlowEnabled":false,"publicClient":false,"serviceAccountsEnabled":false,"standardFlowEnabled":false}'
+    json_template = '{"attributes":{"client_credentials.use_refresh_token":"false","oauth2.device.authorization.grant.enabled":"false","oauth2.token.exchange.grant.enabled":false,"oidc.ciba.grant.enabled":"false","refresh.token.max.reuse":"0","revoke.refresh.token":"false","use.jwks.string":"false","use.jwks.url":"false","use.refresh.tokens":"false"},"consentRequired":false,"implicitFlowEnabled":false,"publicClient":false,"serviceAccountsEnabled":false,"standardFlowEnabled":false,"webOrigins": ["+"]}'
     new_msg = json.loads(json_template)
     new_msg["defaultClientScopes"] = realm_default_client_scopes
     if "client_name" in msg and msg["client_name"]:
